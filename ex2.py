322fa635b9ff9e271522dadc661793e0ef567fde
fbakalar <fbakalar@fff5562e8198>
2016-07-25 23:22:08 +0000

    Add existing file

---
+++ ex2.py
@@ -0,0 +1,12 @@
+
+# first line is a comment line - so that I can read my program easier
+# then another comment line
+
+print 'I could insert some code like this'
+print "if i put a comment after the statement" #it is ignored also
+
+# you can also diable parts of the code by using comments
+#print 'this line will not run'
+
+print 'but this will run'
+
---
+++ helloworld.py
@@ -0,0 +1,7 @@
+print("Hello, world. Again")
+print("another line")
+print "I like typing this"
+print 'this is fun'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this'
+print "oh well"
