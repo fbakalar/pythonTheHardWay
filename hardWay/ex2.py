8c88bf1e7c9bb26ff12bf83b059a5273cf363f29
BernieBakalar <francis.bakalar@gmail.com>
2016-10-25 23:37:30 -0500

    testing from source tree again

--- hardWay/ex2.py
+++ hardWay/ex2.py
@@ -1,7 +1,10 @@
-
+#############################################
+# 
+# this is exercise 2
 # first line is a comment line - so that I can read my program easier
 # then another comment line
-
+#
+###############################################
 print 'I could insert some code like this'
 print "if i put a comment after the statement" #it is ignored also
 
