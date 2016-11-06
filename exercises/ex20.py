ab7db24f7d7d9debb13ecca6fa4fa4bc3d25c0ce
fbakalar <berniebakalar@yahoo.com>
2016-11-05 23:00:22 -0500

    exercise 20

---
+++ exercises/ex20.py
@@ -0,0 +1,43 @@
+#####################################################
+#
+#   Exercise 20 from Python The Hard Way
+#
+#    Functions and Files
+#   C:\Users\berni\Documents\Source\pythonTheHardWay\exercises
+#
+#####################################################
+
+from sys import argv
+
+script, input_file = argv
+
+# this defines the function print_all
+def print_all(f):
+    print f.read()
+
+def rewind(f):
+    f.seek(0)
+
+def print_a_line(line_count, f):
+    print line_count, f.readline()
+
+current_file = open(input_file)
+
+print "First let's print the whole file:\n"
+
+print_all(current_file)  # this calls print_all
+
+print "Now let's rewind, kind of like a tape."
+
+rewind(current_file)  # set current_file back to the start of file
+
+print "Let's print three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
