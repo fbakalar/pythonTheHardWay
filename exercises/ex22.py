315f325427ead9c01c28bafdcba10780011f71c1
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-11-10 22:26:49 -0600

    added keywords to ex22.py  through exercise 16

---
+++ exercises/ex22.py
@@ -0,0 +1,64 @@
+#####################################################
+#
+#   Exercise 22 from Python The Hard Way
+#
+#    
+#   C:\Users\berni\Documents\Source\pythonTheHardWay\exercises
+#  
+#
+#   What do you know so far?
+#
+#####################################################
+
+'''
+
+this is a list of all the key words and symbols used in
+exercises 1 - 21
+
+print:
+
++ plus
+- minus
+/ slash
+* asterisk
+% percent
+< less-than
+> greater-than
+<= less-than-equal
+>= greater-than-equal
+
+%s
+%d
+%r
+
+True
+False
+
+binary
+
+\t
+\n
+\\
+
+raw_input()
+
+from sys import argv
+sys
+argv
+
+prompt
+
+txt = open(filename)
+
+print txt.read()
+
+FILE OPERATIONS
+----------------
+close -- Closes the file. Like File->Save.. in your editor.
+read -- Reads the contents of the file. You can assign the result to a variable.
+readline -- Reads just one line of a text file.
+truncate -- Empties the file. Watch out if you care about the file.
+write('stuff') -- Writes "stuff" to the file.
+
+'''
+
