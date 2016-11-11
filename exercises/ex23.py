073df351e5ceb5386216df2bf0b06c496e150662
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-11-10 22:58:27 -0600

    started a list of random python code from Github to review

---
+++ exercises/ex23.py
@@ -0,0 +1,46 @@
+#####################################################
+#
+#   Exercise 23 from Python The Hard Way
+#
+#    
+#   C:\Users\berni\Documents\Source\pythonTheHardWay\exercises
+#  
+#
+#   Find some sample code for random PYTHON projects
+#
+#   TODO:
+#   add some write up for each of these
+#
+#####################################################
+
+'''
+
+https://github.com/TheAlgorithms/Python/blob/master/sorts/bubble_sort.py
+
+a Dictionary...
+https://github.com/TheAlgorithms/Python/blob/master/other/Dictionary.txt
+
+
+letter frequency finder..
+https://github.com/TheAlgorithms/Python/blob/master/other/frequency_finder.py
+
+Queue represented by a python list
+https://github.com/TheAlgorithms/Python/blob/master/data_structures/Queue/QueueOnList.py
+
+A curated list of awesome Python frameworks, libraries, software and resources.
+https://github.com/vinta/awesome-python
+
+A collection of design patterns/idioms in Python
+https://github.com/faif/python-patterns
+
+abstract factory
+https://github.com/faif/python-patterns/blob/master/abstract_factory.py
+
+the python data science project
+https://github.com/jakevdp/PythonDataScienceHandbook
+
+stack overflow - parsing data files
+http://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file-in-python
+
+'''
+
