37efbcc1d27befa58f2023425e01729e9b67ca84
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-11-10 22:32:36 -0600

    added keywords for ex17 - ex21

--- exercises/ex22.py
+++ exercises/ex22.py
@@ -8,6 +8,9 @@
 #
 #   What do you know so far?
 #
+#   TODO:
+#   add some write up for each of these
+#
 #####################################################
 
 '''
@@ -59,6 +62,26 @@ read -- Reads the contents of the file. You can assign the result to a variable.
 readline -- Reads just one line of a text file.
 truncate -- Empties the file. Watch out if you care about the file.
 write('stuff') -- Writes "stuff" to the file.
+
+from os.path import exists
+
+indata = in_file.read()
+len(indata)
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+def print_two(*args)
+
+def rewind(f):
+    f.seek(0)
+    
+def print_a_line(line_count, f):
+    print line_count, f.readline()
+    
+    
+return
+
 
 '''
 
