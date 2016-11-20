124c8de82f44ffb31741b78926fe538bcc1a0d0d
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-11-19 23:15:50 -0600

    ex24

---
+++ exercises/ex24.py
@@ -0,0 +1,13 @@
+#####################################################
+#
+#  Exercise 24
+#
+#   MORE PRACTICE
+#
+######################################################
+
+print "Let's practice everything"
+print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs'
+
+print 'some good examples of escape sequences:'
+print 'https://linuxconfig.org/list-of-python-escape-sequence-characters-with-examples' 
