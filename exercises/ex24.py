0429e06451cf8dd0f3eebe6d06aaafee51a55f45
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-11-19 23:32:34 -0600

    ex 24 - escape character examples

--- exercises/ex24.py
+++ exercises/ex24.py
@@ -10,4 +10,8 @@ print "Let's practice everything"
 print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs'
 
 print 'some good examples of escape sequences:'
-print 'https://linuxconfig.org/list-of-python-escape-sequence-characters-with-examples' 
+print 'https://linuxconfig.org/list-of-python-escape-sequence-characters-with-examples'
+
+print "\a" 
+print 
+print u"\u041b" u"\U000001a9"
