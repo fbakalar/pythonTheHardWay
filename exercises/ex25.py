fc64608b0a0002dcebc579252b7ff95c19267ddf
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-11-26 23:15:35 -0600

    more changes to ex25 and initial commit for ex26

--- exercises/ex25.py
+++ exercises/ex25.py
@@ -24,18 +24,19 @@ print sort_words("just a random list of words")
 def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
-    return word
- #   print word
+ #   return word
+    print word
 
-def print_second_word(words2):
+def print_second_word(words):
     """Prints the first word after popping it off."""
-    word = words2.pop(0)
+    word = words.pop(0)
     return word
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
     word = words.pop(-1)
-    return word
+ #   return word
+    print word
 
 sentence = break_words('something or anything else')
 print sentence
@@ -54,11 +55,12 @@ def sort_sentence(sentence):
 print "sort_sentence"
 print sort_sentence(' this is the sentence to use')
 
-def print_first_and_last(sentence):
+def print_first_and_last(sentence):  
+# notice there is no <return> statement needed here
     """Prints the first and last words of the sentence."""
-    words = break_words(sentence)
-    print_first_word(words)
-    print_last_word(words)
+    words2 = break_words(sentence)
+    print_first_word(words2)
+    print_last_word(words2)
 
 def print_first_and_last_sorted(sentence):
     """Sorts the words then prints the first and last one."""
