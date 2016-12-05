ccd162f189561b68466ff416e6b3038335db2cd8
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-12-04 21:31:44 -0600

    nested if statements

--- exercises/ex31.py
+++ exercises/ex31.py
@@ -18,11 +18,28 @@ if door == "1":
     print "1. Take the cake."
     print "2.  Scream at the bear."
 
-bear = raw_input("> ")
+    bear = raw_input("> ")
 
-if bear == "1":
-	print "The bear eats your face off.  Good job!"
-elif bear == "2":
-	print "The bear eats your legs off.  Good job!"
+    if bear == "1":
+	    print "The bear eats your face off.  Good job!"
+    elif bear == "2":
+	    print "The bear eats your legs off.  Good job!"
+    else:
+	    print "Well, doing %s is probably better.  Bear runs away."  % bear
+
+elif door == "2":
+	print "You stare into the endless abyss at Cthulhu's retina."
+	print "1. Blueberries."
+	print "2. Yellow jacket clothespins."
+	print "3. Understanding revolvers yelling melodies."
+
+	insanity = raw_input("> ")
+
+	if insanity == "1" or insanity == "2":
+		print "Your body survives powered by a mind of jello.  God job!"
+	else:
+		print "The insanity rots your eyes into a pool of muck.  Good job!"
+
 else:
-	print "Well, doing %s is probably better.  Bear runs away."  % bear
+	print "You stumble around and fall on a knofe and die.  Good job!"
+
