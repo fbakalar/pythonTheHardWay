011256f783ccae8e4ced9085974fdb23c0c2d7ae
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-12-05 21:26:12 -0600

    started exercise 32

---
+++ exercises/ex32.py
@@ -0,0 +1,11 @@
+#---------------------------------------------------------------------|
+#
+#   Exercise 32:  Loops & Lists
+#
+#
+#  Lists are exactly what their name says: 
+#   a container of things that are organized in order from 
+#   first to last
+#
+#----------------------------------------------------------------------|
+
