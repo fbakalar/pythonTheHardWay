dc42b756174049f60d54d17719defe2830a6ef3c
Francis Bakalar <bernie.bakalar@cmegroup.com>
2016-12-13 22:46:49 -0600

    learning keywords

---
+++ exercises/ex37.py
@@ -0,0 +1,88 @@
+#----------------------------------------------------------|
+#
+#   Exercise 37:  Symbol Review
+#
+#
+#
+#
+#
+#----------------------------------------------------------|
+
+
+'''
+import:
+from:
+global:
+del:
+
+'''
+
+
+# LOGIC
+'''
+and: 
+    the <and> keyword is used if all conditions in a 
+     boolean expression must be met
+     ex:
+         if age < 55 and sex == 'M':...
+
+not:
+    the <not> keyword negates a boolean value
+     test a boolean condition
+     ex:
+        if grade not in grades:...
+
+or:
+    the <or> keyword is used if at least one condition 
+     must be met
+
+is:  
+    tests for object identity / are they identical?
+        eg - whether we are talking about the same object
+        note: multiple variables may refer to same object
+            [] is [] ?  False  - because they are 2 distinct objects
+            
+    == operator tests for equality
+            [] == [] ?  True - because they are equivalent
+               even though they are not the same object
+
+
+assert:
+
+with - as:
+'''
+
+#  IF statements and LOOPS
+'''
+break:
+continue:
+if:
+elif:
+else:
+for:
+in:
+yield:
+pass
+'''
+
+# ERROR and EXCEPTIONs
+'''
+try:
+except:
+finally:
+raise:
+'''
+
+# FUNCTIONS and CLASSES
+'''
+def:
+class:
+return:
+lambda:
+'''
+
+
+
+
+
+
