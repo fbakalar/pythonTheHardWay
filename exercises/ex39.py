5f7cbb3a04bd2326790a286b10de4a5867c02b5d
Francis Bakalar <bernie.bakalar@cmegroup.com>
2017-01-11 22:48:23 -0600

    first commit for ex39  - Dictionaries

---
+++ exercises/ex39.py
@@ -0,0 +1,44 @@
+#---------------------------------------------------------|
+#
+#  Exercise 39  Dictionaries
+#   
+#  a dictionary is an alternative way to store data in python
+#  similar to a list - but - instead of using numbers 
+#   to access the data you can use almost anything
+#
+#  so a <dict> becomes almost like a database for storing  
+#   and organizing data
+#   
+#  looks like a <dict> is a key value pair...
+#
+#
+#
+#---------------------------------------------------------|
+
+
+# create a mapping of state to abbreviation
+
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}        
+
+# create a basic set of states and some cities in them
+
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+
+    
+
+
+
+
+
+
