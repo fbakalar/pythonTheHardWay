6e238dbe9a4f94ab13fef2f34c4244c670f87df6
Francis Bakalar <bernie.bakalar@cmegroup.com>
2017-01-23 22:16:07 -0600

    start of exercise:  Modules review

---
+++ exercises/ex40.py
@@ -0,0 +1,28 @@
+#---------------------------------------------------------|
+#
+#  Exercise 40: Modules, Classes, and Objects   
+#
+#   
+#   
+#   
+#  
+#
+#  
+#  __________________________________________________
+#
+# 
+#
+#---------------------------------------------------------|
+
+
+#  Modules are like Dictionaries
+
+mystuff = {'apple':  "I AM APPLES!"}
+print mystuff['apple']  # prints the value for 'apples'
+
+# Module review
+#  1. A Python file with some functions
+#  2. You import the file
+#  3. You can then access functions or variables
+#     using the . (dot) operator
+
